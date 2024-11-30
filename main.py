3d0b1e3f6977a6a88f84f9207be5b5fd97f86107
zmr66z6xx6 <124332399+zmr66z6xx6@users.noreply.github.com>
2024-11-30 16:37:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ from attack import run_attack
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--content_image_path", type=str, default='./data/content/stop-sign/stop-sign.jpg')
-parser.add_argument("--style_image_path", type=str, default='./data/style/stop-sign/dir1.jpg')
+parser.add_argument("--style_image_path", type=str, default='./data/style/stop-sign/stop-sign.jpg')
 parser.add_argument("--content_seg_path",   dest='content_seg_path',    nargs='?',
                     help="Path to the style segmentation",default='./data/content-mask/stop-sign.jpg')
 parser.add_argument("--style_seg_path",     dest='style_seg_path',      nargs='?',
-                    help="Path to the style segmentation",default='./data/style-mask/dir1.jpg')
-parser.add_argument("--result_dir", type=str, default='./Result/sample6')
-parser.add_argument("--target_label", type=int, default=77)  # 默认目标类别
-parser.add_argument("--targeted_attack", action="store_true",default=True)  # 针对性的攻击
-parser.add_argument("--content_weight", type=float, default=5e0)
-parser.add_argument("--style_weight", type=float, default=3e3)
+                    help="Path to the style segmentation",default='./data/style-mask/stop-sign.jpg')
+parser.add_argument("--result_dir", type=str, default='./Result/sample1')
+parser.add_argument("--target_label", type=int, default=200)  # 默认目标类别，可根据synset.txt进行自主修改
+parser.add_argument("--targeted_attack", action="store_true",default=True)  # 有无目标攻击
+parser.add_argument("--content_weight", type=float, default=9e0)
+parser.add_argument("--style_weight", type=float, default=5e3)
 parser.add_argument("--tv_weight", type=float, default=1e-3)
 parser.add_argument("--attack_weight", type=float, default=5e2)
 parser.add_argument("--learning_rate", type=float, default=1.0)
